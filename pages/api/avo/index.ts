650ff9f5a05eae2a4fdeee22c0e3eca9318c4544
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-05-05 16:38:16 -0400

    first commit

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,6 +1,8 @@
 import { IncomingMessage, ServerResponse } from "http";
+import DB from "@database";
 
 const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
+  const db = new DB();
   response.end(JSON.stringify({ hello: "world" }));
 };
 
