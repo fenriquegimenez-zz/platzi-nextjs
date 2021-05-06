739a4c6422af3083b85110ee4ae0ea20715ef75e
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-05-06 10:13:56 -0400

    product list rendered

--- pages/api/avo/index.js
+++ pages/api/avo/index.js
@@ -3,10 +3,9 @@ import DB from "../../../database/db";
 const allAvos = async (request, response) => {
   const db = new DB();
   const allEntries = await db.getAll();
-  const length = allEntries.length;
   response.statusCode = 200; //ok
   response.setHeader("Content-type", "Application-json");
-  response.end(JSON.stringify({ data: allEntries, length: length }));
+  response.end(JSON.stringify({ data: allEntries }));
 };
 
 export default allAvos;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
+  const [productList, setProductList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    document.title = "Avocado app";
+  }, []);
+
+  useEffect(() => {
+    window
+      .fetch("./api/avo/")
+      .then(response => response.json())
+      .then(({ data }) => {
+        console.log("data: ", data);
+        setProductList(data);
+      })
+      .catch(error => console.error("Error: ", error))
+      .finally(setLoading(false));
+  }, []);
+
   return (
     <div>
-      <Navbar></Navbar>
-      <h1>Hola Platzi! NextJS</h1>
+      <Navbar />
+      <h1>Avocados list</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        productList.map(product => {
+          return (
+            <ul>
+              <li key={product.id}>{product.name}</li>
+            </ul>
+          );
+        })
+      )}
     </div>
   );
 };
